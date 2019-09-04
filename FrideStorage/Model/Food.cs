7d73f47e3b4d77bab4964b5beadce889ec1787b9
Rasmus Kristensen <rasmuskristensen1995@hotmail.com>
2019-09-04 10:29:24 +0200

    Added property for the food in the frezzer

---
+++ FrideStorage/Model/Food.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrideStorage.Model
+{
+    class Food
+    {
+        public string Type      { get; set; }
+
+        public string Animal    { get; set; }
+
+        public string Name      { get; set; }
+
+        public int Weight       { get; set; }
+
+        public int Amount       { get; set; }
+
+        public DateTime Date    { get; set; }
+
+        public int Drawer       { get; set; }
+    }
+}
